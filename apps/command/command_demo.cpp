d284b45e826b98c480d5acaa2aa2bb30a455cfbc
kenler <jianping2014@126.com>
2018-11-13 11:24:20 +0800

    modify pattern 'command'

--- apps/command/command_demo.cpp
+++ apps/command/command_demo.cpp
@@ -5,10 +5,14 @@
 // #include "open_command.h"
 #include "paste_command.h"
 int main(int argc, char* argv[]) {
-    gof::Application::Ptr app_ptr(new gof::Application("command demo"));
-    gof::Document::Ptr doc_ptr(new gof::Document("C:/kenler/Desktop/1.txt"));
-    // gof::Command::Ptr command_ptr(new gof::OpenCommand(doc_ptr));
-    gof::Command::Ptr command_ptr(new gof::PasteCommand(doc_ptr));
+    gof::Application::Ptr app_ptr;
+    app_ptr.reset(new gof::Application("command demo"));
+    gof::Document::Ptr doc_ptr;
+    gof::Command::Ptr command_ptr;
+    /*...You could do many operations here...*/
+    doc_ptr.reset(new gof::Document("C:/kenler/Desktop/1.txt"));
+    // command_ptr.reset(new gof::OpenCommand(doc_ptr));
+    command_ptr.reset(new gof::PasteCommand(doc_ptr));
     app_ptr->operate(command_ptr);
     return 0;
 }
