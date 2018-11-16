c3de614a1262ca535c60d12643fea375d07b711a
kenler <jianping2014@126.com>
2018-11-16 15:31:39 +0800

    add pattern 'mediator'

--- apps/mediator/src/landlord.cpp
+++ apps/mediator/src/landlord.cpp
@@ -11,9 +11,9 @@ void Landlord::setMediator(const std::shared_ptr<Mediator>& mediator_ptr) {
 }
 void Landlord::sendMessage(const std::string& message) {
 	// Note: Can't use "Landlord::Ptr(this)" to replace "_this_ptr"
-	/* Because this is common pointer, but Landlord::Ptr(this) is smart pointer.
-	* when u define this, count of the "smart pointer to 'this' object" is only one
-	* Landlord::Ptr(this) is a local object. So when leave the function, this would be released.
+	/* Because 'this' is common pointer, but 'Landlord::Ptr(this)' is smart pointer.
+	* when u define 'Renter::Ptr(this)', count of the "smart pointer to 'this' object" is only one
+	* and Landlord::Ptr(this) is a local object. So when leaving the function, 'this' object would be released.
 	* We don't want to see that.
 	*/
 	_mediator_ptr->send(_this_ptr, "Landlord(" + _name + "): " + message);
--- apps/mediator/src/renter.cpp
+++ apps/mediator/src/renter.cpp
@@ -11,9 +11,9 @@ void Renter::setMediator(const std::shared_ptr<Mediator>& mediator_ptr) {
 }
 void Renter::sendMessage(const std::string& message) {
 	// Note: Can't use "Renter::Ptr(this)" to replace "_this_ptr"
-	/* Because this is common pointer, but Renter::Ptr(this) is smart pointer.
-	 * when u define this, count of the "smart pointer to 'this' object" is only one
-	 * Renter::Ptr(this) is a local object. So when leave the function, this would be released. 
+	/* Because 'this' is common pointer, but 'Renter::Ptr(this)' is smart pointer.
+	 * when u define 'Renter::Ptr(this)', count of the "smart pointer to 'this' object" is only one
+	 * and Renter::Ptr(this) is a local object. So when leaving the function, 'this' object would be released. 
 	 * We don't want to see that.
 	 */
 	_mediator_ptr->send(_this_ptr, "Renter(" + _name + "): " + message);
