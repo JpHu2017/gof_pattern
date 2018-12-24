143a47ba7efa59249425484ee2f4cdb82fc7f32f
kenler <jianping2014@12.com>
2018-12-24 16:24:36 +0800

    update

--- apps/iterator/iterator_demo.cpp
+++ apps/iterator/iterator_demo.cpp
@@ -4,17 +4,15 @@
 #include "iterator.hpp"
 #include "mylist.hpp"
 #include "mylist_iterator.hpp"
-void print(gof::Iterator<gof::Employee>& list_iter) {
-	for (list_iter.first(); !list_iter.isDone(); list_iter.next()) {
-		list_iter.currentItem().print();
-	}
-}
 int main(int argc, char* argv[]) {
     gof::MyList<gof::Employee> employee_list;
     employee_list.push_back(gof::Employee("Anna"));
     employee_list.push_back(gof::Employee("Brown"));
     employee_list.push_back(gof::Employee("Cindy"));
     employee_list.push_back(gof::Employee("David"));
-    print(gof::MyListIterator<gof::Employee>(&employee_list));
+    gof::MyListIterator<gof::Employee> list_iter(&employee_list);
+    for (list_iter.first(); !list_iter.isDone(); list_iter.next()) {
+		list_iter.currentItem().print();
+	}
     return 0;
 }
